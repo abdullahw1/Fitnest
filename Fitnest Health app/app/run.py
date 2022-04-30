32ec58f290a6c12ede0eb47eb50f5fca081f0094
abdullahw1 <90009221+abdullahw1@users.noreply.github.com>
2022-04-30 15:58:12 -0700

    Add files via upload

--- Fitnest Health app/app/run.py
+++ Fitnest Health app/app/run.py
@@ -4,12 +4,17 @@ import webbrowser
 from myapp import myapp_obj, db
 
 DEBUG = False
-PORT_NUMBER = 5005
+PORT_NUMBER = 5012
 
 
 
 # Create *.db file from schema (if doesn't exists)
-db.create_all()
-# Launch webbrowser after 1 seconds
+try:
+    db.create_all()
+except:
+    pass
+
+
 # Run flask app server
+
 myapp_obj.run(debug=DEBUG, port=PORT_NUMBER)
