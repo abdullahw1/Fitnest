155f08b302315b0381808017fd29ee16b26b2f08
abdullahw1 <90009221+abdullahw1@users.noreply.github.com>
2022-05-08 18:23:33 -0700

    Update __init__.py

--- Fitnest Health Applic/app/myapp/__init__.py
+++ Fitnest Health Applic/app/myapp/__init__.py
@@ -15,7 +15,7 @@ myapp_obj.config.from_mapping(
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db'),
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 )
-myapp_obj.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'lux'
+myapp_obj.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'cosmo'
 
 # Install bootstrap extension
 bootstrap = Bootstrap(myapp_obj)
