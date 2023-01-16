3db0c42b6392d3b91f995d46f9f845b036ac4596
abdullahw1 <90009221+abdullahw1@users.noreply.github.com>
2023-01-15 18:19:08 -0800

    Update run.py

--- Fitnest Health Applic/app/run.py
+++ Fitnest Health Applic/app/run.py
@@ -1,12 +1,10 @@
-#!/usr/bin/env python3
 import threading
 import webbrowser
 from myapp import myapp_obj, db
 
 DEBUG = False
+PORT_NUMBER = 5012
 
-def launch_browser():
-    webbrowser.open(f'http://localhost:{PORT_NUMBER}', new=0)
 
 
 # Create *.db file from schema (if doesn't exists)
@@ -15,10 +13,7 @@ try:
 except:
     pass
 
-# Disabled autolaunch browser to deploy heroku
-#if not DEBUG:
-#    threading.Timer(1, launch_browser).start()
 
 # Run flask app server
-if __name__ == '__main__':
-    myapp_obj.run(debug=DEBUG)
+
+myapp_obj.run(debug=DEBUG, port=PORT_NUMBER)
